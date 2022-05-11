4a354a16c43faa034c541606da870f74800d8ca3
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-05-12 01:53:34 +0400

    3d commit

--- Notes-app.py
+++ Notes-app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 def make_database():
         # connects to a database file or creates it
@@ -16,3 +17,30 @@ def make_database():
         connection.commit()
         # close the connection
         connection.close()
+
+def insert_data_to_database(note_text: str):
+        connection = sqlite3.connect('notes.db')
+        print("Connected to database...")
+        curs = connection.cursor()
+        date = str(datetime.datetime.now())[:10]
+        curs.execute(f"INSERT INTO notes VALUES ('{date}', '{note_text}', '{date}')")
+        connection.commit()
+        print("Inserted new data...")
+        print("Connection closed!")
+
+def insert_many_data(notes: list[str]):
+        connection = sqlite3.connect('notes.db')
+        print("Connected to database...")
+        curs = connection.cursor()
+        date = str(datetime.datetime.now())[:10]
+
+        notes_tuples = [(date, text, date) for text in notes]
+
+        # execute and executemany are different
+        # 3 question marks means that we add only 3 tuples
+        curs.executemany(f"INSERT INTO notes VALUES (?,?,?)", notes_tuples)
+        
+        connection.commit()
+        print("Inserted a lot of data...")
+        print("Connection closed!")
+
