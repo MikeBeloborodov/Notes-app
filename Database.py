7f6485cbea3fca0c16a0633bcfbcab333d198ed8
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-05-12 05:53:48 +0400

    Beta version with add and view notes only

--- Database.py
+++ Database.py
@@ -1,8 +1,9 @@
 import os
 import sqlite3
 import datetime
+import msvcrt
 
-def make_database():
+def create_database() -> bool:
     if not os.path.exists("notes.sql"):
         conn = sqlite3.connect("notes.sql")
         curs = conn.cursor()
@@ -17,6 +18,8 @@ def make_database():
 
         conn.commit()
         conn.close()
+        return True
+    else: return False
 
 def add_note_to_db(text: str):
     conn = sqlite3.connect("notes.sql")
@@ -37,10 +40,80 @@ def retrieve_all_notes() -> list:
     curs = conn.cursor()
 
     curs.execute("""
-            SELECT *
+            SELECT rowid, *
             FROM notes
     """)
     notes = curs.fetchall()
     conn.close()
 
     return notes
+
+def press_any_key():
+    print("Press any key...")
+    msvcrt.getch()
+
+def clear_console():
+    os.system('CLS')
+
+def user_choice_view_notes():
+        clear_console()
+        # returns true if database didn't exist before
+        if create_database():
+            print("I've just created a new database for you!")
+            print("It's empty now, let's add a note!")
+            press_any_key()
+            return
+            
+        notes = retrieve_all_notes()
+        if not notes:
+            print("Database is empty :(")
+            print("Please add some notes!")
+            press_any_key()
+            return
+
+        clear_console()
+        for note in notes:
+            print("---------------------------")
+            print(f"Note id: {note[0]}")
+            print(f"Date added: {note[1]}")
+            print(f"Text: {note[2]}")
+        press_any_key()
+        return
+
+def user_choice_add_new_note():
+        clear_console()
+        # returns true if database didn't exist before
+        if create_database():
+            print("I just created a new database for you!")
+            print("It's empty now, let's add a note!")
+
+        print("What should I save in a note?")
+        new_note_text = input("Your text here: ")
+        try:
+            add_note_to_db(new_note_text)
+            print("Note added succesfully!")
+            press_any_key()
+        except:
+            print("Something went wrong... Please try again.")
+            press_any_key()
+            return
+
+def user_choice_exit_program():
+        clear_console()
+        print("Goodbye!")
+        press_any_key()
+        exit()
+
+def handle_user_choice(user_choice: str):
+    valid_choices = ['1', '2', '3']
+    if user_choice not in valid_choices:
+        print("Error. Please try again.")
+        print("Press any key...")
+        press_any_key()
+        return
+    elif user_choice == '1':
+        user_choice_add_new_note()
+    elif user_choice == '2':
+        user_choice_view_notes()
+    elif user_choice == '3':
+        user_choice_exit_program()
--- Notes-app.py
+++ Notes-app.py
@@ -1,5 +1,16 @@
-import sqlite3
-import datetime
 import Database
+from Database import clear_console
+import msvcrt
 
+def main_loop():
+        while True:
+                clear_console()
+                print("Welcome to the notes program!")
+                print("1 - Add a note")
+                print("2 - View notes")
+                print("3 - Exit")
+                user_choice = input("Enter your number here: ")
+                Database.handle_user_choice(user_choice)
 
+if __name__ == "__main__":
+        main_loop()
